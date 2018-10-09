9a808551873261a264c15342570e63ea52db1f0b
Юрий Мельников <myi@katek.by>
2018-10-09 13:54:16 +0300

    delete PSQL context

--- F1H/Models/EFRepository.cs
+++ F1H/Models/EFRepository.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace F1H.Models
 {
@@ -13,36 +10,37 @@ namespace F1H.Models
             context = ctx;
         }
 
-        public IQueryable<Chassi> Chassis => context.Chassis;
-        public IQueryable<Country> Country => context.Country;
-        public IQueryable<DescriptionResult> DescriptionResults => context.DescriptionResults;
-        public IQueryable<DNQ> DNQs => context.DNQs;
-        public IQueryable<Engine> Engines => context.Engines;
-        public IQueryable<Fail> Fails => context.Fails;
-        public IQueryable<FastLap> FastLaps => context.FastLaps;
-        public IQueryable<GP> GPs => context.GPs;
-        public IQueryable<GPResult> GPResults => context.GPResults;
-        public IQueryable<ImageGP> Images => context.Images;
-        public IQueryable<ImageGPConfiguration> ImagesGPConfigurations => context.ImagesGPConfigurations;
-        public IQueryable<ImageGPFlag> ImagesGPFlags => context.ImagesGPFlags;
-        public IQueryable<ImageGPLiver> ImagesGPLivers => context.ImagesGPLivers;
-        public IQueryable<ImageGPRacers> ImagesGPRacers => context.ImagesGPRacers;
-        public IQueryable<LeaderLap> LeaderLaps => context.LeaderLaps;
-        public IQueryable<Manufacturer> Manufacturers => context.Manufacturers;
-        public IQueryable<Participant> Participants => context.Participants;
-        public IQueryable<Pit> Pits => context.Pits;
-        public IQueryable<Qualification> Qualifications => context.Qualifications;
-        public IQueryable<Racer> Racers => context.Racers;
-        public IQueryable<Season> Seasons => context.Seasons;
-        public IQueryable<StartPositionType> StartPositionTypes => context.StartPositionTypes;
-        public IQueryable<TeamName> TeamNames => context.TeamNames;
-        public IQueryable<Team> Teams => context.Teams;
-        public IQueryable<Track> Tracks => context.Tracks;
-        public IQueryable<TrackСonfiguration> GetTrackСonfigurations => context.GetTrackСonfigurations;
         public IQueryable<TypeCalculate> TypeCalculates => context.TypeCalculates;
         public IQueryable<TypeDNQ> TypeDNQs => context.TypeDNQs;
         public IQueryable<TypeFail> TypeFails => context.TypeFails;
         public IQueryable<TypeFinish> TypeFinishes => context.TypeFinishes;
-        public IQueryable<Tyre> Tyres => context.Tyres;
+
+        //public IQueryable<Chassi> Chassis => context.Chassis;
+        //public IQueryable<Country> Country => context.Country;
+        //public IQueryable<DescriptionResult> DescriptionResults => context.DescriptionResults;
+        //public IQueryable<DNQ> DNQs => context.DNQs;
+        //public IQueryable<Engine> Engines => context.Engines;
+        //public IQueryable<Fail> Fails => context.Fails;
+        //public IQueryable<FastLap> FastLaps => context.FastLaps;
+        //public IQueryable<GP> GPs => context.GPs;
+        //public IQueryable<GPResult> GPResults => context.GPResults;
+        //public IQueryable<ImageGP> Images => context.Images;
+        //public IQueryable<ImageGPConfiguration> ImagesGPConfigurations => context.ImagesGPConfigurations;
+        //public IQueryable<ImageGPFlag> ImagesGPFlags => context.ImagesGPFlags;
+        //public IQueryable<ImageGPLiver> ImagesGPLivers => context.ImagesGPLivers;
+        //public IQueryable<ImageGPRacers> ImagesGPRacers => context.ImagesGPRacers;
+        //public IQueryable<LeaderLap> LeaderLaps => context.LeaderLaps;
+        //public IQueryable<Manufacturer> Manufacturers => context.Manufacturers;
+        //public IQueryable<Participant> Participants => context.Participants;
+        //public IQueryable<Pit> Pits => context.Pits;
+        //public IQueryable<Qualification> Qualifications => context.Qualifications;
+        //public IQueryable<Racer> Racers => context.Racers;
+        //public IQueryable<Season> Seasons => context.Seasons;
+        //public IQueryable<StartPositionType> StartPositionTypes => context.StartPositionTypes;
+        //public IQueryable<TeamName> TeamNames => context.TeamNames;
+        //public IQueryable<Team> Teams => context.Teams;
+        //public IQueryable<Track> Tracks => context.Tracks;
+        //public IQueryable<TrackСonfiguration> GetTrackСonfigurations => context.GetTrackСonfigurations;
+        //public IQueryable<Tyre> Tyres => context.Tyres;
     }
 }
